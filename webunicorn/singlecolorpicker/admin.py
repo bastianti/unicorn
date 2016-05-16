26445635044b622d3699d14ceebd2773312b89e9
root <root@bastipi.fritz.box>
2016-05-16 07:40:08 +0000

    Added settings.py, w/o critical data

--- webunicorn/singlecolorpicker/admin.py
+++ webunicorn/singlecolorpicker/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import RGBColor, Brightness
+from .models import RGBColor, Brightness, LastColorValue
 
 admin.site.register(RGBColor)
 admin.site.register(Brightness)
